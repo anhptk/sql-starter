d46c4df3b515ad92d32d9c32502420439b987b4e
Anh Pham <anh.is.a.dev@gmail.com>
2024-07-26 14:58:22 +0700

    style: + update N_5 format

--- queries/N_5.sql
+++ queries/N_5.sql
@@ -9,32 +9,33 @@ SELECT
     (
         SELECT row_to_json(f)
         FROM (
-                 SELECT id, name, mime_type, key, url
-                 FROM files
-                 WHERE id = m.poster_id
-             ) f
+                SELECT id, name, mime_type, key, url
+                FROM files
+                WHERE id = m.poster_id
+            ) f
     ) AS poster,
     (
         SELECT row_to_json(p)
         FROM (
-                 SELECT id, first_name, last_name
-                 FROM people
-                 WHERE id = m.director_id
-             ) p
+                SELECT id, first_name, last_name
+                FROM people
+                WHERE id = m.director_id
+            ) p
     ) AS director
+
 FROM movies m
 -- Country id is 1
 WHERE country_id = 1
-    --   Release date is after 2022-01-01
-  AND release_date >= '2022-01-01'
-    --   Duration is greater than 2hr 15min
-  AND duration > 135
+    -- Release date is after 2022-01-01
+    AND release_date >= '2022-01-01'
+    -- Duration is greater than 2hr 15min
+    AND duration > 135
 
-    --   Genre is Action or Drama
-  AND EXISTS (
-    SELECT 1
-    FROM movies_genres mg
-             JOIN genres g ON mg.genre_id = g.id
-    WHERE mg.movie_id = m.id
-      AND g.name IN ('Action', 'Drama')
-);
+    -- Genre is Action or Drama
+    AND EXISTS (
+        SELECT 1
+        FROM movies_genres mg
+        JOIN genres g ON mg.genre_id = g.id
+        WHERE mg.movie_id = m.id
+        AND g.name IN ('Action', 'Drama')
+    );
